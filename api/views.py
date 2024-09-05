1bdbe11749314233ed5a53c53cfb0ae94da9b467
project2479 <jaywrsnp6@gmail.com>
2024-09-05 11:23:33 +0800

    first

---
+++ api/views.py
@@ -0,0 +1,46 @@
+# views.py
+from rest_framework import generics, permissions, views
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import status
+from .serializers import UserSerializer
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import FileResponse, Http404
+from django.shortcuts import get_object_or_404
+from django.views import View
+from rest_framework.decorators import api_view
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        # Extract user data from request
+        user_data = self.request.data
+        username = user_data.get('username')
+        email = user_data.get('email')
+        mobile_num = user_data.get('mobile_num')
+       
+        # Check if the username, email, or mobile number already exists
+        if User.objects.filter(username=username).exists():
+            raise ValidationError({'username': 'A user with this Mobile Number already exists.'})
+
+        if User.objects.filter(email=email).exists():
+            raise ValidationError({'email': 'A user with this email already exists.'})
+
+        
+        # Save the user and profile
+        serializer.save()
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
